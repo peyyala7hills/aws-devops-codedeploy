aa74a0396369ba0a1b500cc5823be19e2e0bca06
peyyala7hills <101161227+peyyala7hills@users.noreply.github.com>
2023-11-01 19:44:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull yedukondalu212/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 yedukondalu212/simple-python-flask-app
+docker run -d -p -t sample 5000:5000 yedukondalu212/simple-python-flask-app
